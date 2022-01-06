e830390d63f7733fd72e1c39e1a87bf309f160a5
Mani5871 <manideepak650@gmail.com>
2022-01-06 10:15:48 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// a5df608aa4bc4acd84b2bc6683b75d74
+
+export default class App extends Component {
+  name = "Mani Deepak";
+  render() {
+    return (
+      <div>
+        My first class based component {this.name}
+      </div>
+    )
+  }
 }
-
-export default App;
